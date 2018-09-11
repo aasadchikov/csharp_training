3dbf36df1002cdf42054b9c2eb27072cf04b2870
Anton Sadchikov <aasadchikov@gmail.com>
2018-09-11 05:12:45 +0300

    All changes on 11.09.2018 until Lesson 2.1

---
+++ addressbook-web-tests/addressbook-web-tests/GroupCreationTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NUnit.Framework;
+
+namespace WebAddressBookTests
+{
+    [TestFixture]
+    public class GroupCreationTests : TestBase
+    {
+        
+        [Test]
+        public void GroupCreationTest()
+        {
+            GoToHomePage();
+            Login(new AccauntData("admin", "secret"));
+            GoToGroupsPage();
+            InitGroupCreation();
+            GroupData group = new GroupData("aaa");
+            group.Header = "ddd";
+            group.Footer = "fff";       
+            FillGroupForm(group);
+            SubmitGroupCreation();
+            ReturnToGroupPage();
+        }
+
+ 
+    }
+}
---
+++ addressbook-web-tests/addressbook-web-tests/GroupRemovalTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NUnit.Framework;
+
+namespace WebAddressBookTests
+{
+    [TestFixture]
+    public class GroupRemovalTests : TestBase
+    {
+        
+
+        [Test]
+        public void GroupRemovalTest()
+        {
+            GoToHomePage();
+            Login(new AccauntData("admin", "secret"));
+            GoToGroupsPage();
+            SelectGroup(1);
+            RemoveGroup();
+            ReturnToGroupPage();
+        }
+
+        
+            
+    }
+}
